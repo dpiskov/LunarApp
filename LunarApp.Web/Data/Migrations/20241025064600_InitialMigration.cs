c194bca1f59795629ff0182a5322c27d554af180
dpiskov <dpiskov02@gmail.com>
2024-10-25 10:04:52 +0300

    Added InitialMigration

---
+++ LunarApp.Web/Data/Migrations/20241025064600_InitialMigration.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LunarApp.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notebooks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Notebook Identifier"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, comment: "Notebook title")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notebooks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Folders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Folder Identifier"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false, comment: "Folder title"),
+                    NotebookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Identifier of a notebook"),
+                    ParentFolderId = table.Column<Guid>(type: "uniqueidentifier", nullable: true, comment: "Identifier of a parent folder")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Folders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Folders_Folders_ParentFolderId",
+                        column: x => x.ParentFolderId,
+                        principalTable: "Folders",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Folders_Notebooks_NotebookId",
+                        column: x => x.NotebookId,
+                        principalTable: "Notebooks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Note Identifier"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, comment: "Note title"),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false, comment: "Note body"),
+                    NotebookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Identifier of a notebook"),
+                    FolderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "Identifier of a folder"),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "The date the note was created on"),
+                    LastSaved = table.Column<DateTime>(type: "datetime2", nullable: false, comment: "The date the note was last saved on")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notes_Folders_FolderId",
+                        column: x => x.FolderId,
+                        principalTable: "Folders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Notes_Notebooks_NotebookId",
+                        column: x => x.NotebookId,
+                        principalTable: "Notebooks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folders_NotebookId",
+                table: "Folders",
+                column: "NotebookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folders_ParentFolderId",
+                table: "Folders",
+                column: "ParentFolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_FolderId",
+                table: "Notes",
+                column: "FolderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_NotebookId",
+                table: "Notes",
+                column: "NotebookId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Folders");
+
+            migrationBuilder.DropTable(
+                name: "Notebooks");
+        }
+    }
+}
