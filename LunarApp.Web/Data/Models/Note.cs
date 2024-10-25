346e2ab949a85243a436728498bf712c9951927b
dpiskov <dpiskov02@gmail.com>
2024-10-25 08:20:19 +0300

    Created Note entity model

---
+++ LunarApp.Web/Data/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static LunarApp.Web.Common.ValidationConstants;
+
+namespace LunarApp.Web.Data.Models
+{
+    public class Note
+    {
+        
+    }
+}
