7db612afe516c92ca95de1e7aa05f71aa26c2f74
dpiskov <dpiskov02@gmail.com>
2024-10-25 09:09:24 +0300

    Added DbSets for Note, Folder and Notebook in ApplicationDbContext

--- LunarApp.Web/Data/ApplicationDbContext.cs
+++ LunarApp.Web/Data/ApplicationDbContext.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LunarApp.Web.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LunarApp.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Folder> Folders { get; set; }
+        public DbSet<Notebook> Notebooks { get; set; }
     }
 }
