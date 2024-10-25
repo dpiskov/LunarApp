47be3f53696bfa664c945fdbe6b9a8f8d2cf3e0f
dpiskov <dpiskov02@gmail.com>
2024-10-25 09:00:38 +0300

    Filled the Notebook entity model

--- LunarApp.Web/Data/Models/Notebook.cs
+++ LunarApp.Web/Data/Models/Notebook.cs
@@ -1,6 +1,19 @@
-﻿namespace LunarApp.Web.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using static LunarApp.Web.Common.ValidationConstants;
+
+namespace LunarApp.Web.Data.Models
 {
     public class Notebook
     {
+        [Required]
+        [Comment("Notebook Identifier")]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(NotebookTitleMaxLength)]
+        [Comment("Notebook title")]
+        public required string Title { get; set; }
+        public ICollection<Folder> Folders { get; set; }
+        public ICollection<Note> Notes { get; set; }
     }
 }
