9ae454bea84e2b086f8541a0a45ee433e2f8a2eb
dpiskov <dpiskov02@gmail.com>
2024-10-25 08:51:16 +0300

    Filled the Folder entity model

--- LunarApp.Web/Data/Models/Folder.cs
+++ LunarApp.Web/Data/Models/Folder.cs
@@ -1,6 +1,31 @@
-﻿namespace LunarApp.Web.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+using static LunarApp.Web.Common.ValidationConstants;
+
+namespace LunarApp.Web.Data.Models
 {
     public class Folder
     {
+        [Key]
+        [Comment("Folder Identifier")]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(FolderTitleMaxLength)]
+        [Comment("Folder title")]
+        public required string Title { get; set; }
+        [Required]
+        [Comment("Identifier of a notebook")]
+        public Guid NotebookId { get; set; }
+        [ForeignKey(nameof(NotebookId))]
+        public required Notebook Notebook { get; set; }
+        [Required]
+        [Comment("Identifier of a parent folder")]
+        public Guid ParentFolderId { get; set; }
+        [ForeignKey(nameof(ParentFolderId))]
+        public required Folder ParentFolder { get; set; }
+        public required ICollection<Folder> Folders { get; set; }
+        public required ICollection<Note> Notes { get; set; }
     }
 }
