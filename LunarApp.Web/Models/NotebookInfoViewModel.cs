f6b369c922dd9564a845cdba9e0efaafbf9ee3a2
dpiskov <dpiskov02@gmail.com>
2024-10-28 16:08:08 +0200

    Added NotebookInfoViewModel

---
+++ LunarApp.Web/Models/NotebookInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LunarApp.Web.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LunarApp.Web.Models
+{
+    public class NotebookInfoViewModel
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+
+    }
+}
