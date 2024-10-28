4b9357d56c282cbb64e18c36abd9600539e746e6
dpiskov <dpiskov02@gmail.com>
2024-10-28 14:35:29 +0200

    Added Index action for NotebookController

---
+++ LunarApp.Web/Controllers/NotebookController.cs
@@ -0,0 +1,24 @@
+﻿using LunarApp.Web.Data;
+using LunarApp.Web.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LunarApp.Web.Controllers
+{
+    public class NotebookController(ApplicationDbContext context) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var model = await context.Notebooks
+                .Select(nb => new Notebook()
+                {
+                    Id = nb.Id,
+                    Title = nb.Title
+                })
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(model);
+        }
+    }
+}
