983446f3b5ddd8d0a749a5ceec06fe32b264d87b
dpiskov <dpiskov02@gmail.com>
2024-10-28 16:08:25 +0200

    Added NotebookViewModel

---
+++ LunarApp.Web/Models/NotebookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static LunarApp.Web.Common.ValidationConstants;
+
+namespace LunarApp.Web.Models
+{
+    public class NotebookViewModel
+    {
+        [Required]
+        [StringLength(NotebookTitleMaxLength, MinimumLength = NotebookTitleMinLength)]
+        public required string Title { get; set; }
+    }
+}
