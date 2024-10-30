2242f38abe09830e2a6341a23fcb2d260361919d
dpiskov <dpiskov02@gmail.com>
2024-10-30 09:19:51 +0200

    Create FolderController.cs

---
+++ LunarApp.Web/Controllers/FolderController.cs
@@ -0,0 +1,16 @@
+﻿using LunarApp.Web.Data;
+using LunarApp.Web.Data.Models;
+using LunarApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LunarApp.Web.Controllers
+{
+    public class FolderController(ApplicationDbContext context) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
