c71c9d1f5bce97337db97bdd1fb343e1777f6ce5
dpiskov <dpiskov02@gmail.com>
2024-11-11 09:20:55 +0200

    Create FolderDeleteViewModel.cs

---
+++ LunarApp.Web/Models/FolderDeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LunarApp.Web.Models
+{
+    public class FolderDeleteViewModel
+    {
+        //public Guid Id { get; set; }
+        public required string Title { get; set; }
+        [Required]
+        public Guid NotebookId { get; set; }
+        public Guid? ParentFolderId { get; set; }
+    }
+}
