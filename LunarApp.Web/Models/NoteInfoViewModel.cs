d71e1b23e27b44bb33f1866db60510ca42035b33
dpiskov <dpiskov02@gmail.com>
2024-11-13 15:27:46 +0200

    Create NoteInfoViewModel.cs

---
+++ LunarApp.Web/Models/NoteInfoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LunarApp.Web.Models
+{
+    public class NoteInfoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string BodyPreview { get; set; }
+        public Guid? FolderId { get; set; }
+    }
+}
