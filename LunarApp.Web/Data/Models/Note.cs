ee13e8174b43ace987664dd778cb5ccbfca5e275
dpiskov <dpiskov02@gmail.com>
2024-11-13 15:26:58 +0200

    Update Note.cs

--- LunarApp.Web/Data/Models/Note.cs
+++ LunarApp.Web/Data/Models/Note.cs
@@ -9,23 +9,25 @@ namespace LunarApp.Web.Data.Models
     {
         [Key]
         [Comment("Note Identifier")]
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         [Required]
         [MaxLength(NoteTitleMaxLength)]
         [Comment("Note title")]
         public required string Title { get; set; }
+        [MaxLength(NoteBodyMaxLength)]
         [Comment("Note body")]
-        public string Body { get; set; } = string.Empty;
+        public string? Body { get; set; }
         [Required]
         [Comment("Identifier of a notebook")]
         public Guid NotebookId { get; set; }
         [ForeignKey(nameof(NotebookId))]
         public required Notebook Notebook { get; set; }
-        [Required]
+        public Notebook Notebook { get; set; }
+        //[Required]
         [Comment("Identifier of a folder")]
         public Guid? FolderId { get; set; }
         [ForeignKey(nameof(FolderId))]
-        public required Folder Folder { get; set; }
+        public Folder Folder { get; set; }
         [Required]
         [Comment("The date the note was created on")]
         public DateTime DateCreated { get; set; }
