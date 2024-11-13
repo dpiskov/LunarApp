ba4422b751983647cd89d42c8f34aeae7cf7cd9e
dpiskov <dpiskov02@gmail.com>
2024-11-13 15:20:41 +0200

    Create NoteController.cs

---
+++ LunarApp.Web/Controllers/NoteController.cs
@@ -0,0 +1,13 @@
+﻿using Humanizer;
+using LunarApp.Web.Data;
+using LunarApp.Web.Data.Models;
+using LunarApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LunarApp.Web.Controllers
+{
+    public class NoteController(ApplicationDbContext context) : Controller
+    {
+    }
+}
