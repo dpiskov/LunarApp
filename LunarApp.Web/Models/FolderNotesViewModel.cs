83ba05ef4557917533c6bd5a37468b3f6debfb60
dpiskov <dpiskov02@gmail.com>
2024-11-13 15:27:21 +0200

    Create FolderNotesViewModel.cs

---
+++ LunarApp.Web/Models/FolderNotesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LunarApp.Web.Models
+{
+    public class FolderNotesViewModel
+    {
+        public IEnumerable<FolderInfoViewModel> Folders { get; set; }
+        public IEnumerable<NoteInfoViewModel> Notes { get; set; }
+    }
+}
