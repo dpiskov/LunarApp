44ec15b855fc2e44c31ae377ac4004a5192feb81
dpiskov <dpiskov02@gmail.com>
2024-11-14 08:49:31 +0200

    Added Description to Notebook entity model

--- LunarApp.Web/Data/Models/Notebook.cs
+++ LunarApp.Web/Data/Models/Notebook.cs
@@ -13,6 +13,9 @@ namespace LunarApp.Web.Data.Models
         [MaxLength(NotebookTitleMaxLength)]
         [Comment("Notebook title")]
         public required string Title { get; set; }
+        [MaxLength(NotebookDescriptionMaxLength)]
+        [Comment("Notebook description")]
+        public string? Description { get; set; }
         public ICollection<Folder> Folders { get; set; } = new List<Folder>();
         public ICollection<Note> Notes { get; set; } = new List<Note>();
     }
