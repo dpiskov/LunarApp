f7f0b45b76c3ee6c8da575496e209d602ec12b48
dpiskov <dpiskov02@gmail.com>
2024-11-14 08:49:06 +0200

    Create 20241114061001_AddDescriptionForNotebook.cs

---
+++ LunarApp.Web/Data/Migrations/20241114061001_AddDescriptionForNotebook.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LunarApp.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionForNotebook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Notebooks",
+                type: "nvarchar(max)",
+                maxLength: 20000,
+                nullable: true,
+                comment: "Notebook description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Notebooks");
+        }
+    }
+}
