8ec6ee7de963a6fc79e1635f251bb66d68ed1e63
dpiskov <dpiskov02@gmail.com>
2024-11-16 13:23:54 +0200

    Create FolderDetailsViewModel.cs

---
+++ LunarApp.Web/Models/FolderDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static LunarApp.Web.Common.ValidationConstants;
+
+namespace LunarApp.Web.Models
+{
+    public class FolderDetailsViewModel
+    {
+        [MaxLength(FolderDescriptionMaxLength)]
+        public string? Description { get; set; }
+        [Required]
+        public Guid NotebookId { get; set; }
+        [Required]
+        public Guid ParentFolderId { get; set; }
+    }
+}
