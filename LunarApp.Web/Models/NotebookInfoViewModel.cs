c40d67a9a90d08f628e40ef6bf3cee59ae3b4dc0
dpiskov <dpiskov02@gmail.com>
2024-11-19 16:42:50 +0200

    Update NotebookInfoViewModel for Notebook

--- LunarApp.Web/Models/NotebookInfoViewModel.cs
+++ LunarApp.Web/Models/NotebookInfoViewModel.cs
@@ -1,14 +1,8 @@
-﻿using LunarApp.Web.Data.Models;
-
-namespace LunarApp.Web.Models
+﻿namespace LunarApp.Web.Models
 {
     public class NotebookInfoViewModel
     {
         public Guid Id { get; set; }
         public required string Title { get; set; }
-        public ICollection<Folder> Folders { get; set; } = new List<Folder>();
-        //public ICollection<Note> Notes { get; set; } = new List<Note>();
-        public IEnumerable<NoteInfoViewModel> Notes {get; set; }
-
     }
 }
