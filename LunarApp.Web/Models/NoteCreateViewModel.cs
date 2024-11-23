e675c7f7e8fceda5fac6fe9b2a1beacc3a1db1eb
dpiskov <dpiskov02@gmail.com>
2024-11-23 10:53:12 +0200

    Create NoteCreateViewModel.cs

---
+++ LunarApp.Web/Models/NoteCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using static LunarApp.Web.Common.ValidationConstants;
+using System.ComponentModel.DataAnnotations;
+
+namespace LunarApp.Web.Models
+{
+    public class NoteCreateViewModel
+    {
+        [Required]
+        [MinLength(NoteTitleMinLength)]
+        [MaxLength(NoteTitleMaxLength)]
+        public required string Title { get; set; }
+
+        [MinLength(NoteBodyMinLength)]
+        [MaxLength(NoteBodyMaxLength)]
+        public string? Body { get; set; }
+
+        [Required]
+        public Guid NotebookId { get; set; }
+        public Guid? ParentFolderId { get; set; }
+        public Guid? FolderId { get; set; }
+    }
+}
