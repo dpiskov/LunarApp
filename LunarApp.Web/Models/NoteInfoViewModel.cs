f5cc0cae6fe0ad4067a2b1e0d3de4a0c1584ff3d
dpiskov <dpiskov02@gmail.com>
2024-11-23 10:53:39 +0200

    Update NoteInfoViewModel.cs

--- LunarApp.Web/Models/NoteInfoViewModel.cs
+++ LunarApp.Web/Models/NoteInfoViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace LunarApp.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LunarApp.Web.Models
 {
     public class NoteInfoViewModel
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string BodyPreview { get; set; }
+        public required string Title { get; set; }
+        [Required]
+        public Guid NotebookId { get; set; }
         public Guid? FolderId { get; set; }
     }
 }
