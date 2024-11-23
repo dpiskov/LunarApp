6c872969f168b8a8abd1c76a75c60c842f8140e6
dpiskov <dpiskov02@gmail.com>
2024-11-23 10:53:22 +0200

    Create NoteEditViewModel.cs

---
+++ LunarApp.Web/Models/NoteEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LunarApp.Web.Models
+{
+    public class NoteEditViewModel
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public string? Body { get; set; }
+        [Required]
+        public Guid NotebookId { get; set; }
+        public Guid? ParentFolderId { get; set; }
+        public Guid? FolderId { get; set; }
+        public DateTime LastSaved { get; set; }
+    }
+}
