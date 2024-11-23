c88eb6bf9e697f53c39d2caff88336cc2e09c415
dpiskov <dpiskov02@gmail.com>
2024-11-23 10:52:05 +0200

    Create FolderCreateViewModel.cs

---
+++ LunarApp.Web/Models/FolderCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static LunarApp.Web.Common.ValidationConstants;
+
+namespace LunarApp.Web.Models
+{
+    public class FolderCreateViewModel
+    {
+        [Required]
+        [StringLength(FolderTitleMaxLength, MinimumLength = FolderTitleMinLength)]
+        public required string Title { get; set; }
+
+        [Required]
+        public Guid NotebookId { get; set; }
+        public Guid? ParentFolderId { get; set; }
+        public Guid? FolderId { get; set; }
+        public bool IsMadeDirectlyFromNotebook { get; set; }
+    }
+}
