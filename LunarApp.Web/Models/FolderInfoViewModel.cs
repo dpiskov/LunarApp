f11f7474a307b8308d64f552c603dc92b2f78a49
dpiskov <dpiskov02@gmail.com>
2024-11-23 10:52:39 +0200

    Update FolderInfoViewModel.cs

--- LunarApp.Web/Models/FolderInfoViewModel.cs
+++ LunarApp.Web/Models/FolderInfoViewModel.cs
@@ -1,7 +1,4 @@
-﻿using LunarApp.Web.Data.Models;
-using System.ComponentModel.DataAnnotations;
-
-namespace LunarApp.Web.Models
+﻿namespace LunarApp.Web.Models
 {
     public class FolderInfoViewModel
     {
@@ -9,8 +6,5 @@ namespace LunarApp.Web.Models
         public required string Title { get; set; }
         public Guid NotebookId { get; set; }
         public Guid? ParentFolderId { get; set; }
-
-        public ICollection<Folder> ChildrenFolders { get; set; } = new List<Folder>();
-        public ICollection<Note> Notes { get; set; } = new List<Note>();
     }
 }
