0a81056d3599403f12b18ed8046a7e10ab656738
dpiskov <dpiskov02@gmail.com>
2024-11-23 10:52:14 +0200

    Update FolderDeleteViewModel.cs

--- LunarApp.Web/Models/FolderDeleteViewModel.cs
+++ LunarApp.Web/Models/FolderDeleteViewModel.cs
@@ -4,10 +4,10 @@ namespace LunarApp.Web.Models
 {
     public class FolderDeleteViewModel
     {
-        //public Guid Id { get; set; }
         public required string Title { get; set; }
         [Required]
         public Guid NotebookId { get; set; }
         public Guid? ParentFolderId { get; set; }
+        public Guid FolderId { get; set; }
     }
 }
