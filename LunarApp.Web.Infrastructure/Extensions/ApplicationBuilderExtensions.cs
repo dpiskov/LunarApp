3bae72a7440de1857a8fc06bb8f1e7549601da98
dpiskov <dpiskov02@gmail.com>
2024-11-28 08:57:33 +0200

    Create ApplyMigrations method

--- LunarApp.Web.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ LunarApp.Web.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -7,5 +7,13 @@ namespace LunarApp.Web.Infrastructure.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
+        public static IApplicationBuilder ApplyMigrations(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            ApplicationDbContext dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            dbContext.Database.Migrate();
+
+            return app;
+        }
     }
 }
