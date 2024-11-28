ecc41a1eebbecf8168e1c57b07953cedb3cf9389
dpiskov <dpiskov02@gmail.com>
2024-11-28 08:49:52 +0200

    Create EntityValidationMessages.cs

---
+++ LunarApp.Common/EntityValidationMessages.cs
@@ -0,0 +1,22 @@
+﻿namespace LunarApp.Common
+{
+    public static class EntityValidationMessages
+    {
+        public static class Note
+        {
+            public const string NoteTitleRequiredMessage = "Note title is required!";
+        }
+
+        public static class Folder
+        {
+            public const string FolderTitleRequiredMessage = "Folder title is required!";
+
+        }
+
+        public static class Notebook
+        {
+            public const string NotebookTitleRequiredMessage = "Notebook title is required!";
+
+        }
+    }
+}
