bec8f35e17d23ef9f1b14872ea3102a1c7ea1cd6
dpiskov <dpiskov02@gmail.com>
2024-11-28 08:49:59 +0200

    Create ApplicationUser.cs

---
+++ LunarApp.Data.Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LunarApp.Data.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ApplicationUser()
+        {
+            // ReSharper disable once VirtualMemberCallInConstructor
+            this.Id = Guid.NewGuid();
+        }
+
+        //TODO: Add more properties to user
+
+        //public virtual ICollection<ApplicationUserMovie> ApplicationUserMovies { get; set; }
+        //    = new HashSet<ApplicationUserMovie>();
+
+        //public virtual ICollection<Ticket> Tickets { get; set; }
+        //    = new HashSet<Ticket>();
+    }
+}
+
+
