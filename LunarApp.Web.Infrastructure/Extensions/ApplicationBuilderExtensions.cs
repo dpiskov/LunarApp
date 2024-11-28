550c9027c2f2ce83f96d1ca29fd0675d2a88ac1b
dpiskov <dpiskov02@gmail.com>
2024-11-28 08:57:12 +0200

    Create ApplicationBuilderExtensions.cs

---
+++ LunarApp.Web.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,11 @@
+﻿using LunarApp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LunarApp.Web.Infrastructure.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+    }
+}
