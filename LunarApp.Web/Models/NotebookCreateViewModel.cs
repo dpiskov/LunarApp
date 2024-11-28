9f54011a703e634ae4340700698e68151e92e48f
dpiskov <dpiskov02@gmail.com>
2024-11-28 09:19:36 +0200

    Update NotebookCreateViewModel.cs

--- LunarApp.Web/Models/NotebookCreateViewModel.cs
+++ LunarApp.Web/Models/NotebookCreateViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-using static LunarApp.Web.Common.ValidationConstants;
+using static LunarApp.Common.ValidationConstants.Notebook;
+using static LunarApp.Common.EntityValidationMessages.Notebook;
 
 namespace LunarApp.Web.Models
 {
     public class NotebookCreateViewModel
     {
-        [Required]
+        [Required(ErrorMessage = NotebookTitleRequiredMessage)]
         [StringLength(NotebookTitleMaxLength, MinimumLength = NotebookTitleMinLength)]
         public required string Title { get; set; }
     }
