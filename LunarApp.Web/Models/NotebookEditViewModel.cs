96c1620ca87dbb231b8d4e4b2af9419768ca7aea
dpiskov <dpiskov02@gmail.com>
2024-11-28 09:19:46 +0200

    Update NotebookEditViewModel.cs

--- LunarApp.Web/Models/NotebookEditViewModel.cs
+++ LunarApp.Web/Models/NotebookEditViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static LunarApp.Common.ValidationConstants.Notebook;
+using static LunarApp.Common.EntityValidationMessages.Notebook;
 
 namespace LunarApp.Web.Models
 {
     public class NotebookEditViewModel
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = NotebookTitleRequiredMessage)]
+        [StringLength(NotebookTitleMaxLength, MinimumLength = NotebookTitleMinLength)]
         public required string Title { get; set; }
 
     }
