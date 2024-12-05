41347db61f13f343113d8c7cdd9afae3646a7a53
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:48:33 +0200

    Added Post Create action method for TagController

--- LunarApp.Web/Controllers/TagController.cs
+++ LunarApp.Web/Controllers/TagController.cs
@@ -52,5 +52,32 @@ namespace LunarApp.Web.Controllers
 
             return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TagCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Tag tag = new Tag
+                {
+                    Name = model.Name
+                };
+
+                context.Tags.Add(tag);
+                await context.SaveChangesAsync();
+
+                if (model.FolderId != Guid.Empty && model.FolderId != null &&
+                    model.ParentFolderId != Guid.Empty && model.ParentFolderId != null)
+                {
+                    return Redirect($"~/Tag?notebookId={model.NotebookId}&parentFolderId={model.ParentFolderId}&folderId={model.FolderId}&noteId={model.NoteId}");
+                }
+                else if (model.FolderId != Guid.Empty && model.FolderId != null)
+                {
+                    return Redirect($"~/Tag?notebookId={model.NotebookId}&folderId={model.FolderId}&noteId={model.NoteId}");
+                }
+            }
+
+            return View(model);
+        }
     }
 }
