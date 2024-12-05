5186d91afae32ab592ebcc1ce20d136188cd0726
dpiskov <dpiskov02@gmail.com>
2024-12-05 23:39:33 +0200

    Create FolderHelperService.cs

---
+++ LunarApp.Services.Data/FolderHelperService.cs
@@ -0,0 +1,19 @@
+﻿using LunarApp.Data.Models;
+using LunarApp.Data.Repository.Interfaces;
+using LunarApp.Services.Data.Interfaces;
+
+namespace LunarApp.Services.Data
+{
+    public class FolderHelperService(IRepository<Folder, Guid> folderRepository) : IFolderHelperService
+    {
+        public Task<Guid> GetParentFolderIdAsync(Guid? parentFolderId, Guid newParentFolderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(Guid newParentFolderId, Guid newFolderId)> GetFolderAndParentIdsAsync(Guid? parentFolderId, Guid newParentFolderId, Guid newFolderId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
