7d1c314cc47b33fa0b521d0a56f80f21ba99ac79
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:26:21 +0200

    Create Tag Entity Model

---
+++ LunarApp.Data.Models/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+using static LunarApp.Common.ValidationConstants.Tag;
+
+namespace LunarApp.Data.Models
+{
+    public class Tag
+    {
+        [Key]
+        [Comment("Tag Identifier")]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(TagNameMaxLength)]
+        [Comment("Tag name")]
+        public required string Name { get; set; }
+    }
+}
