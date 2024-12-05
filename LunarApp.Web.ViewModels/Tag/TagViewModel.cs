d881319ff27e58c111827db8ec93ecbf5690cff4
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:29:24 +0200

    Create TagViewModel.cs

---
+++ LunarApp.Web.ViewModels/Tag/TagViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LunarApp.Web.ViewModels.Tag
+{
+    public class TagViewModel
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public Guid? NotebookId { get; set; }
+        public Guid? ParentFolderId { get; set; }
+        public Guid? FolderId { get; set; }
+        public Guid? NoteId { get; set; }
+    }
+}
