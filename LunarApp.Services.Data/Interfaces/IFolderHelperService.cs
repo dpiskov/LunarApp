08aae174f9a5ba11fa028c1922764157b2272779
dpiskov <dpiskov02@gmail.com>
2024-12-05 15:00:12 +0200

    Create IFolderHelperService.cs

---
+++ LunarApp.Services.Data/Interfaces/IFolderHelperService.cs
@@ -0,0 +1,11 @@
+﻿namespace LunarApp.Services.Data.Interfaces
+{
+    public interface IFolderHelperService
+    {
+        Task<Guid> GetParentFolderIdAsync(Guid? parentFolderId, Guid newParentFolderId);
+
+        Task<(Guid newParentFolderId, Guid newFolderId)> GetFolderAndParentIdsAsync(Guid? parentFolderId,
+            Guid newParentFolderId, Guid newFolderId);
+        Task<string?> GetParentFolderTitleAsync(Guid folderId);
+    }
+}
