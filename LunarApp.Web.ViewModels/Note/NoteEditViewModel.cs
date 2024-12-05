3a6070ea26e50a793d37e5a57b381a231f09e950
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:57:25 +0200

    Update NoteEditViewModel.cs

--- LunarApp.Web.ViewModels/Note/NoteEditViewModel.cs
+++ LunarApp.Web.ViewModels/Note/NoteEditViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using static LunarApp.Common.ValidationConstants.Note;
 using static LunarApp.Common.EntityValidationMessages.Note;
+using LunarApp.Web.ViewModels.Tag;
 
 namespace LunarApp.Web.ViewModels.Note
 {
@@ -12,10 +13,14 @@ namespace LunarApp.Web.ViewModels.Note
         public required string Title { get; set; }
         [MaxLength(NoteBodyMaxLength)]
         public string? Body { get; set; }
+        public Guid? SelectedTagId { get; set; }
+        public IEnumerable<TagViewModel> Tags { get; set; } = new List<TagViewModel>();
+
         [Required]
         public Guid NotebookId { get; set; }
         public Guid? ParentFolderId { get; set; }
         public Guid? FolderId { get; set; }
+
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public DateTime LastSaved { get; set; } = DateTime.Now;
     }
