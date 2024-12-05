65eec5d31c4a1c4fe288bfca85419748a2aa7336
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:29:14 +0200

    Create TagEditViewModel.cs

---
+++ LunarApp.Web.ViewModels/Tag/TagEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static LunarApp.Common.ValidationConstants.Tag;
+
+
+namespace LunarApp.Web.ViewModels.Tag
+{
+    public class TagEditViewModel
+    {
+        public Guid Id {get; set; }
+        [Required]
+        [StringLength(TagNameMaxLength, MinimumLength = TagNameMinLength)]
+        public required string Name { get; set; }
+        public Guid? NotebookId { get; set; }
+        public Guid? ParentFolderId { get; set; }
+        public Guid? FolderId { get; set; }
+        public Guid? NoteId { get; set; }
+    }
+}
