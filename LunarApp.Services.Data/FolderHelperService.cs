4c57b884881ef3edcced05d08243c2d91712de15
dpiskov <dpiskov02@gmail.com>
2024-12-05 23:40:57 +0200

    Implement GetParentFolderIdAsync in FolderHelperService

--- LunarApp.Services.Data/FolderHelperService.cs
+++ LunarApp.Services.Data/FolderHelperService.cs
@@ -1,14 +1,26 @@
 ﻿using LunarApp.Data.Models;
 using LunarApp.Data.Repository.Interfaces;
 using LunarApp.Services.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace LunarApp.Services.Data
 {
     public class FolderHelperService(IRepository<Folder, Guid> folderRepository) : IFolderHelperService
     {
-        public Task<Guid> GetParentFolderIdAsync(Guid? parentFolderId, Guid newParentFolderId)
+        public async Task<Guid> GetParentFolderIdAsync(Guid? parentFolderId, Guid newParentFolderId)
         {
-            throw new NotImplementedException();
+            Folder? folder = await folderRepository
+                .GetAllAttached()
+                .SelectMany(f => f.ChildrenFolders)
+                .Where(f => f.Id == parentFolderId)
+                .FirstOrDefaultAsync();
+
+            if (folder is not null && folder.ParentFolderId.HasValue && folder.ParentFolderId != Guid.Empty)
+            {
+                newParentFolderId = folder.ParentFolderId.Value;
+            }
+
+            return newParentFolderId;
         }
 
         public Task<(Guid newParentFolderId, Guid newFolderId)> GetFolderAndParentIdsAsync(Guid? parentFolderId, Guid newParentFolderId, Guid newFolderId)
