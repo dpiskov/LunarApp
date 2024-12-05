65579a4fb41e82f25d75426045bc5e995662e61c
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:57:31 +0200

    Update NotebookDetailsViewModel.cs

--- LunarApp.Web.ViewModels/Notebook/NotebookDetailsViewModel.cs
+++ LunarApp.Web.ViewModels/Notebook/NotebookDetailsViewModel.cs
@@ -6,6 +6,8 @@ namespace LunarApp.Web.ViewModels.Notebook
     public class NotebookDetailsViewModel
     {
         public Guid Id { get; set; }
+        [Required]
+        public required string Title { get; set; }
         [MaxLength(NotebookDescriptionMaxLength)]
         public string? Description { get; set; }
     }
