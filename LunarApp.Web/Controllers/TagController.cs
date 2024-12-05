17ecd699ed17d4caebb42b2dcbd7419915effd17
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:31:33 +0200

    Create TagController.cs

---
+++ LunarApp.Web/Controllers/TagController.cs
@@ -0,0 +1,15 @@
+﻿using LunarApp.Data;
+using LunarApp.Data.Models;
+using LunarApp.Web.ViewModels.Tag;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LunarApp.Web.Controllers
+{
+    [Authorize]
+    public class TagController(ApplicationDbContext context) : Controller
+    {
+        
+    }
+}
