b1db9e8495af3dc8dd1f2df29d07384e1ee7882e
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:29:10 +0200

    Create TagCreateViewModel.cs

---
+++ LunarApp.Web.ViewModels/Tag/TagCreateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static LunarApp.Common.ValidationConstants.Tag;
+
+
+namespace LunarApp.Web.ViewModels.Tag
+{
+    public class TagCreateViewModel
+    {
+        [Required]
+        [StringLength(TagNameMaxLength, MinimumLength = TagNameMinLength)]
+        public required string Name { get; set; }
+        public Guid? NotebookId { get; set; }
+        public Guid? ParentFolderId { get; set; }
+        public Guid? FolderId { get; set; }
+        public Guid? NoteId { get; set; }
+    }
+}
