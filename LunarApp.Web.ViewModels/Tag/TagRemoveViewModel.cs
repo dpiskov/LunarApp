5eb4a33cd00181dd8d94ec73b4fef03803b8ca64
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:29:19 +0200

    Create TagRemoveViewModel.cs

---
+++ LunarApp.Web.ViewModels/Tag/TagRemoveViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LunarApp.Web.ViewModels.Tag
+{
+    public class TagRemoveViewModel
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public Guid NotebookId { get; set; }
+        public Guid? ParentFolderId { get; set; }
+        public Guid? FolderId { get; set; }
+        public Guid NoteId { get; set; }
+    }
+}
