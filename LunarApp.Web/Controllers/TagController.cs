69da8d18711e693732959e4ae47c5feecb3c5b03
dpiskov <dpiskov02@gmail.com>
2024-12-05 08:47:19 +0200

    Added Index action method for TagController

--- LunarApp.Web/Controllers/TagController.cs
+++ LunarApp.Web/Controllers/TagController.cs
@@ -10,6 +10,25 @@ namespace LunarApp.Web.Controllers
     [Authorize]
     public class TagController(ApplicationDbContext context) : Controller
     {
-        
+        public async Task<IActionResult> Index(Guid? notebookId, Guid? parentFolderId, Guid? folderId, Guid? noteId)
+        {
+            IEnumerable<TagViewModel> tags = await context.Tags
+                .Select(t => new TagViewModel
+                {
+                    Id = t.Id,
+                    Name = t.Name
+                })
+                .OrderBy(t => t.Name)
+                .AsNoTracking()
+                .ToListAsync();
+            //List<Tag> tags = context.Tags.ToList();
+
+            ViewData["NotebookId"] = notebookId;
+            ViewData["ParentFolderId"] = parentFolderId;
+            ViewData["FolderId"] = folderId;
+            ViewData["NoteId"] = noteId;
+
+            return View(tags);
+        }
     }
 }
