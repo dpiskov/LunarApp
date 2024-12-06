fc43ebe4c3964263e7dc1f951567b13186a4d226
dpiskov <dpiskov02@gmail.com>
2024-12-06 15:06:33 +0200

    Update IFolderService.cs

--- LunarApp.Services.Data/Interfaces/IFolderService.cs
+++ LunarApp.Services.Data/Interfaces/IFolderService.cs
@@ -7,12 +7,12 @@ namespace LunarApp.Services.Data.Interfaces
     {
         Task<FolderNotesViewModel> IndexGetAllFoldersAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId);
         Task<FolderCreateViewModel> GetAddFolderModelAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId);
-        Task<(bool isSuccess, string? errorMessage, Folder? folder)> AddFolderAsync(FolderCreateViewModel model);
+        Task<(bool isSuccess, string? errorMessage)> AddFolderAsync(FolderCreateViewModel model);
         Task<(FolderCreateViewModel model, Guid newParentFolderId)> GetAddSubfolderModelAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId);
         Task<(FolderDeleteViewModel? model, Guid newParentFolderId)> GetFolderForDeleteByIdAsync(Guid notebookId, Guid? parentFolderId, Guid folderId);
         Task DeleteFolderWithChildrenAsync(Guid folderId);
-        Task<(FolderEditViewModel model, Guid newParentFolderId)> GetFolderForEditByIdAsync(Guid notebookId, Guid? parentFolderId, Guid folderId);
-        Task<(bool isEdited, Folder? parentFolder)> EditFolderAsync(FolderEditViewModel model);
+        Task<(FolderEditViewModel? model, Guid newParentFolderId)> GetFolderForEditByIdAsync(Guid notebookId, Guid? parentFolderId, Guid folderId);
+        Task<(bool isEdited, Folder? parentFolder)> EditFolderAsync(FolderEditViewModel? model);
         Task<(FolderDetailsViewModel? model, Guid newParentFolderId)> GetFolderDetailsByIdAsync(Guid notebookId, Guid? parentFolderId, Guid folderId);
         Task<(bool isEdited, Folder? parentFolder)> EditDetailsFolderAsync(FolderDetailsViewModel? model);
 
