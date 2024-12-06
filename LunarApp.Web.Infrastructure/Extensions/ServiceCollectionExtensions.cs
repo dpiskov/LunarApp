26a94b9cf31ccf0da3d34b9d864400121592a1ab
dpiskov <dpiskov02@gmail.com>
2024-12-06 14:36:24 +0200

    Create RegisterRepositories extension method in ServiceCollectionExtensions

--- LunarApp.Web.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ LunarApp.Web.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,55 @@
-﻿namespace LunarApp.Web.Infrastructure.Extensions
+﻿using System.Reflection;
+using LunarApp.Data.Models;
+using LunarApp.Data.Repository;
+using LunarApp.Data.Repository.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LunarApp.Web.Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        
+        public static void RegisterRepositories(this IServiceCollection services, Assembly modelsAssembly)
+        {
+            // TODO: Rewrite the implementation in such way that the user must create a single class for every repository
+            Type[] typesToExclude = new Type[] { typeof(ApplicationUser) };
+            Type[] modelTypes = modelsAssembly
+                .GetTypes()
+                .Where(t => !t.IsAbstract &&
+                                 !t.IsInterface &&
+                                 !t.Name
+                                      .ToLower().EndsWith("attribute"))
+                .ToArray();
+
+            foreach (Type type in modelTypes)
+            {
+                if (!typesToExclude.Contains(type))
+                {
+                    Type repositoryInterface = typeof(IRepository<,>);
+                    Type repositoryInstanceType = typeof(BaseRepository<,>);
+
+                    PropertyInfo idPropInfo = type
+                        .GetProperties()
+                        .Where(p => p.Name.ToLower() == "id")
+                        .SingleOrDefault();
+
+                    Type[] constructArgs = new Type[2];
+                    constructArgs[0] = type;
+
+                    if (idPropInfo == null)
+                    {
+                        constructArgs[1] = typeof(object);
+                    }
+                    else
+                    {
+                        constructArgs[1] = idPropInfo.PropertyType;
+                    }
+
+                    repositoryInterface = repositoryInterface.MakeGenericType(constructArgs);
+                    repositoryInstanceType = repositoryInstanceType.MakeGenericType(constructArgs);
+
+                    services.AddScoped(repositoryInterface, repositoryInstanceType);
+                }
+            }
+        }
     }
 }
