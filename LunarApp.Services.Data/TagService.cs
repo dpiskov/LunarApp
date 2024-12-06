618db98ad7392d30ab038c12a731471d804bdad6
dpiskov <dpiskov02@gmail.com>
2024-12-06 14:42:32 +0200

    Create TagService.cs

---
+++ LunarApp.Services.Data/TagService.cs
@@ -0,0 +1,48 @@
+﻿using LunarApp.Data.Models;
+using LunarApp.Data.Repository.Interfaces;
+using LunarApp.Services.Data.Interfaces;
+using LunarApp.Web.ViewModels.Tag;
+using Microsoft.EntityFrameworkCore;
+
+namespace LunarApp.Services.Data
+{
+    public class TagService(
+        IRepository<Tag, Guid> tagRepository
+        ) : ITagService
+    {
+        public Task<IEnumerable<TagViewModel>> IndexGetAllTagsOrderedByNameAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TagCreateViewModel> GetCreateTagAsync(Guid? notebookId, Guid? parentFolderId, Guid? folderId, Guid? noteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateTagAsync(TagCreateViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TagEditViewModel?> GetTagForEditByIdAsync(Guid? notebookId, Guid? parentFolderId, Guid? folderId, Guid? noteId, Guid tagId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditTagAsync(TagEditViewModel? model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TagRemoveViewModel?> GetTagForDeleteByIdAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId, Guid noteId, Guid tagId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTagAsync(Guid tagId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
