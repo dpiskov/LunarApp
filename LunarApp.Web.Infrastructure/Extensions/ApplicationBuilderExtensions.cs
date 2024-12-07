6b81f222225cebdc3138d956f4c214d1f628d93e
dpiskov <dpiskov02@gmail.com>
2024-12-07 19:53:26 +0200

    Add CreateAdminUserAsync in ApplicationBuilderExtensions

--- LunarApp.Web.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ LunarApp.Web.Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -1,7 +1,11 @@
 ﻿using LunarApp.Data;
+using LunarApp.Data.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+
+using static LunarApp.Common.ApplicationConstants;
 
 namespace LunarApp.Web.Infrastructure.Extensions
 {
@@ -14,6 +18,25 @@ namespace LunarApp.Web.Infrastructure.Extensions
             dbContext.Database.Migrate();
 
             return app;
+        }
+
+        private static async Task<ApplicationUser> CreateAdminUserAsync(string email, string username, string password,
+            IUserStore<ApplicationUser> userStore, UserManager<ApplicationUser> userManager)
+        {
+            ApplicationUser applicationUser = new ApplicationUser
+            {
+                Email = email
+            };
+
+            await userStore.SetUserNameAsync(applicationUser, username, CancellationToken.None);
+            IdentityResult result = await userManager.CreateAsync(applicationUser, password);
+
+            if (result.Succeeded == false)
+            {
+                throw new InvalidOperationException($"Error occured while registering {AdminRoleName} user!");
+            }
+
+            return applicationUser;
         }
     }
 }
