521a1f845605451f7999c983b5737aefd271f81e
dpiskov <dpiskov02@gmail.com>
2024-12-07 20:05:39 +0200

    Create Post AssignRole action method for UserManagementController

--- LunarApp.Web/Areas/Admin/Controllers/UserManagementController.cs
+++ LunarApp.Web/Areas/Admin/Controllers/UserManagementController.cs
@@ -17,5 +17,30 @@ namespace LunarApp.Web.Areas.Admin.Controllers
             IEnumerable<AllUsersViewModel> allUsers = await userService.GetAllUsersAsync();
             return View(allUsers);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(string userId, string role)
+        {
+            Guid userGuid = Guid.Empty;
+            if (IsGuidValid(userId, ref userGuid) == false)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            bool userExists = await userService.UserExistsByIdAsync(userGuid);
+
+            if (userExists == false)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            bool assignResult = await userService.AssignUserToRoleAsync(userGuid, role);
+            if (assignResult == false)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
