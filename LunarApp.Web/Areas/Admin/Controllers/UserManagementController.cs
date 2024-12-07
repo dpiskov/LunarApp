985fb4506871ae3abc59f1b76f437f7843c234e4
dpiskov <dpiskov02@gmail.com>
2024-12-07 20:00:40 +0200

    Create UserManagementController for Admin

---
+++ LunarApp.Web/Areas/Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,15 @@
+﻿using LunarApp.Services.Data.Interfaces;
+using LunarApp.Web.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static LunarApp.Common.ApplicationConstants;
+
+namespace LunarApp.Web.Areas.Admin.Controllers
+{
+    [Area(AdminRoleName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class UserManagementController(IUserService userService) : BaseController
+    {
+        
+    }
+}
