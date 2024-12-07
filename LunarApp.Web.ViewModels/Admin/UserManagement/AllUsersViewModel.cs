86c93d11fa573d7101bd51d0173e8264c1105676
dpiskov <dpiskov02@gmail.com>
2024-12-07 19:56:26 +0200

    Create AllUsersViewModel.cs

---
+++ LunarApp.Web.ViewModels/Admin/UserManagement/AllUsersViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LunarApp.Web.ViewModels.Admin.UserManagement
+{
+    public class AllUsersViewModel
+    {
+        public required string Id { get; set; }
+        public string? Email { get; set; }
+        public required IEnumerable<string> Roles {get; set; }
+    }
+}
