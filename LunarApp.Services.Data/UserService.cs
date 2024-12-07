ff43a8c3ce8348d377c815821f5b825ba40c8b42
dpiskov <dpiskov02@gmail.com>
2024-12-07 20:00:27 +0200

    Create UserService.cs

---
+++ LunarApp.Services.Data/UserService.cs
@@ -0,0 +1,36 @@
+﻿using LunarApp.Data.Models;
+using LunarApp.Services.Data.Interfaces;
+using LunarApp.Web.ViewModels.Admin.UserManagement;
+using Microsoft.AspNetCore.Identity;
+
+namespace LunarApp.Services.Data
+{
+    public class UserService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole<Guid>> roleManager)
+        : BaseService, IUserService
+    {
+        public Task<IEnumerable<AllUsersViewModel>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExistsByIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AssignUserToRoleAsync(Guid userId, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserRoleAsync(Guid userId, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
