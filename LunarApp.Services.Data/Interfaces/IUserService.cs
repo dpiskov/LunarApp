168cc42b124519f6a901e836d8d299aba51f15af
dpiskov <dpiskov02@gmail.com>
2024-12-07 19:55:18 +0200

    Create IUserService.cs

---
+++ LunarApp.Services.Data/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using LunarApp.Web.ViewModels.Admin.UserManagement;
+
+namespace LunarApp.Services.Data.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<AllUsersViewModel>> GetAllUsersAsync();
+
+        Task<bool> UserExistsByIdAsync(Guid userId);
+
+        Task<bool> AssignUserToRoleAsync(Guid userId, string roleName);
+
+        Task<bool> RemoveUserRoleAsync(Guid userId, string roleName);
+
+        Task<bool> DeleteUserAsync(Guid userId);
+    }
+}
