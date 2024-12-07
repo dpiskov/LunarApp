044c918c8c1165fb15863ba650421e90e916a488
dpiskov <dpiskov02@gmail.com>
2024-12-07 19:56:42 +0200

    Create HomeController for Admin Area

---
+++ LunarApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using static LunarApp.Common.ApplicationConstants;
+
+namespace LunarApp.Web.Areas.Admin.Controllers
+{
+    [Area(AdminRoleName)]
+    [Authorize(Roles = AdminRoleName)]
+    public class HomeController : Controller
+    {
+        [Area(AdminRoleName)]
+        [Authorize(Roles = AdminRoleName)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
