b86f9e9c82bebb583ff96ebe297917612cc86234
dpiskov <dpiskov02@gmail.com>
2024-12-08 11:34:50 +0200

    Create IBaseService.cs

---
+++ LunarApp.Services.Data/Interfaces/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using LunarApp.Web.ViewModels.Folder;
+
+namespace LunarApp.Services.Data.Interfaces
+{
+    public interface IBaseService
+    {
+        Task<FolderNotesViewModel> GetFilteredNotesAsyncByNotebookId(Guid notebookId, Guid? parentFolderId, Guid? folderId, string? searchQuery, string? tagFilter);
+        Task<FolderNotesViewModel> GetFilteredNotesAsync(Guid? parentFolderId, Guid? folderId, string? searchQuery, string? tagFilter);
+        Task<IEnumerable<string>> GetAllTagsAsync();
+    }
+}
