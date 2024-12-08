bb0749df953605ef1a7e30ff51a46b6b792e24e1
dpiskov <dpiskov02@gmail.com>
2024-12-08 11:48:23 +0200

    Create SearchFilterViewModel.cs

---
+++ LunarApp.Web.ViewModels/SearchFilterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using LunarApp.Web.ViewModels.Folder;
+using LunarApp.Web.ViewModels.Notebook;
+
+namespace LunarApp.Web.ViewModels
+{
+    public class SearchFilterViewModel
+    {
+        public IEnumerable<NotebookInfoViewModel>? Notebooks { get; set; }
+
+        public FolderNotesViewModel? FolderNotes { get; set; }
+
+        public string? SearchQuery { get; set; }
+        public string? TagFilter { get; set; }
+        public IEnumerable<string>? AllTags { get; set; }
+
+    }
+}
