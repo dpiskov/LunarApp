8825217cb295c9bee44e72191477d4b06ef20f8a
dpiskov <dpiskov02@gmail.com>
2024-12-10 08:57:55 +0200

    Update NoteService.cs

--- LunarApp.Services.Data/NoteService.cs
+++ LunarApp.Services.Data/NoteService.cs
@@ -14,9 +14,9 @@ namespace LunarApp.Services.Data
         IRepository<Tag, Guid> tagRepository)
         : INoteService
     {
-        public async Task<NoteCreateViewModel> GetCreateNoteAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId)
+        public async Task<NoteCreateViewModel> GetCreateNoteAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId, Guid userId)
         {
-            List<TagViewModel> tags = await GetAllTagsAsync();
+            List<TagViewModel> tags = await GetAllTagsAsync(userId);
 
             NoteCreateViewModel model = new NoteCreateViewModel
             {
@@ -42,7 +42,7 @@ namespace LunarApp.Services.Data
             return model;
         }
 
-        public async Task<(bool isSuccess, string? errorMessage)> CreateNoteAsync(NoteCreateViewModel model)
+        public async Task<(bool isSuccess, string? errorMessage)> CreateNoteAsync(NoteCreateViewModel model, Guid userId)
         {
             Notebook? notebook = await notebookRepository.GetByIdAsync(model.NotebookId);
             if (notebook == null)
@@ -68,22 +68,23 @@ namespace LunarApp.Services.Data
                 ParentFolderId = model.ParentFolderId,
                 FolderId = model.FolderId,
                 TagId = model.SelectedTagId,
+                UserId = userId,
                 DateCreated = model.DateCreated,
                 LastSaved = model.LastSaved
             };
 
             await noteRepository.AddAsync(note);
 
-            model.Tags = await GetAllTagsAsync();
+            model.Tags = await GetAllTagsAsync(userId);
 
             return (true, null);
         }
 
-        public async Task<NoteDeleteViewModel?> GetNoteForDeleteByIdAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId, Guid noteId)
+        public async Task<NoteDeleteViewModel?> GetNoteForDeleteByIdAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId, Guid noteId, Guid userId)
         {
             NoteDeleteViewModel? model = await noteRepository
                 .GetAllAttached()
-                .Where(n => n.Id == noteId)
+                .Where(n => n.Id == noteId && n.UserId == userId)
                 .AsNoTracking()
                 .Select(n => new NoteDeleteViewModel()
                 {
@@ -91,18 +92,18 @@ namespace LunarApp.Services.Data
                     Title = n.Title,
                     NotebookId = n.NotebookId,
                     ParentFolderId = n.ParentFolderId,
-                    FolderId = n.FolderId
+                    FolderId = n.FolderId,
                 })
                 .FirstOrDefaultAsync();
 
             return model;
         }
 
-        public async Task DeleteNoteAsync(Guid noteId)
+        public async Task DeleteNoteAsync(Guid noteId, Guid userId)
         {
             Note? note = await noteRepository
                 .GetAllAttached()
-                .Where(n => n.Id == noteId)
+                .Where(n => n.Id == noteId && n.UserId == userId)
                 .FirstOrDefaultAsync();
 
             if (note == null)
@@ -113,11 +114,11 @@ namespace LunarApp.Services.Data
             await noteRepository.DeleteAsync(noteId);
         }
 
-        public async Task<NoteEditViewModel?> GetNoteForEditByIdAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId, Guid noteId)
+        public async Task<NoteEditViewModel?> GetNoteForEditByIdAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId, Guid noteId, Guid userId)
         {
             Note? note = await noteRepository
                 .GetAllAttached()
-                .Where(n => n.Id == noteId)
+                .Where(n => n.Id == noteId && n.UserId == userId)
                 .AsNoTracking()
                 .FirstOrDefaultAsync();
 
@@ -126,7 +127,7 @@ namespace LunarApp.Services.Data
                 return null;
             }
 
-            List<TagViewModel> tags = await GetAllTagsAsync();
+            List<TagViewModel> tags = await GetAllTagsAsync(userId);
 
             NoteEditViewModel model = new NoteEditViewModel
             {
@@ -145,14 +146,18 @@ namespace LunarApp.Services.Data
             return model;
         }
 
-        public async Task<bool> EditNoteAsync(NoteEditViewModel? model)
+        public async Task<bool> EditNoteAsync(NoteEditViewModel? model, Guid userId)
         {
             if (model == null || string.IsNullOrWhiteSpace(model.Title))
             {
                 return false;
             }
 
-            Note? note = await noteRepository.GetByIdAsync(model.Id);
+            //Note? note = await noteRepository.GetByIdAsync(model.Id);
+            Note? note = await noteRepository
+                .GetAllAttached()
+                .Where(n => n.Id == model.Id && n.UserId == userId) // Verify user ownership
+                .FirstOrDefaultAsync();
 
             if (note == null)
             {
@@ -169,10 +174,11 @@ namespace LunarApp.Services.Data
             return isEdited;
         }
 
-        public async Task<List<TagViewModel>> GetAllTagsAsync()
+        public async Task<List<TagViewModel>> GetAllTagsAsync(Guid userId)
         {
             return await tagRepository
                 .GetAllAttached()
+                .Where(t => t.UserId == userId)
                 .AsNoTracking()
                 .Select(t => new TagViewModel
                 {
