cafef273ffdd1247c281ac42a8c04bb1c0849892
dpiskov <dpiskov02@gmail.com>
2024-12-10 12:34:20 +0200

    Implement GetCurrentUserId method for BaseController

--- LunarApp.Web/Controllers/BaseController.cs
+++ LunarApp.Web/Controllers/BaseController.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LunarApp.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LunarApp.Web.Controllers
 {
     public class BaseController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public BaseController()
+        {
+
+        }
+
+        public BaseController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        protected Guid GetCurrentUserId()
+        {
+            string? userId = _userManager.GetUserId(User);
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                throw new InvalidOperationException("Unable to retrieve the current user ID.");
+            }
+
+            return Guid.Parse(userId);
+        }
+
         protected bool IsGuidValid(string? id, ref Guid parsedGuid)
         {
             // Non-existing parameter in the URL
