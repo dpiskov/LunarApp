1305515826efa6c47b3f712cd977b434717ef97c
dpiskov <dpiskov02@gmail.com>
2024-12-10 08:58:18 +0200

    Update INotebookService.cs

--- LunarApp.Services.Data/Interfaces/INotebookService.cs
+++ LunarApp.Services.Data/Interfaces/INotebookService.cs
@@ -5,23 +5,23 @@ namespace LunarApp.Services.Data.Interfaces
 {
     public interface INotebookService
     {
-        Task<Notebook?> GetByTitleAsync(string title);
+        Task<Notebook?> GetByTitleAsync(string title, Guid userId);
 
-        Task<IEnumerable<NotebookInfoViewModel>> IndexGetAllOrderedByTitleAsync();
+        Task<IEnumerable<NotebookInfoViewModel>> IndexGetAllOrderedByTitleAsync(Guid userId);
 
-        Task AddNotebookAsync(NotebookCreateViewModel model);
+        Task AddNotebookAsync(NotebookCreateViewModel model, Guid userId);
 
-        Task<NotebookDeleteViewModel?> GetNotebookForDeleteByIdAsync(Guid notebookId);
+        Task<NotebookDeleteViewModel?> GetNotebookForDeleteByIdAsync(Guid notebookId, Guid userId);
 
-        Task DeleteNotebookAsync(Guid notebookId);
+        Task DeleteNotebookAsync(Guid notebookId , Guid userId);
 
-        Task<NotebookEditViewModel?> GetNotebookForEditByIdAsync(Guid notebookId);
+        Task<NotebookEditViewModel?> GetNotebookForEditByIdAsync(Guid notebookId , Guid userId);
 
-        Task<bool> EditNotebookAsync(NotebookEditViewModel? model);
+        Task<bool> EditNotebookAsync(NotebookEditViewModel? model, Guid userId);
 
 
-        Task<NotebookDetailsViewModel?> GetNotebookDetailsByIdAsync(Guid id);
-        Task<bool> EditDetailsNotebookAsync(NotebookDetailsViewModel? model);
+        Task<NotebookDetailsViewModel?> GetNotebookDetailsByIdAsync(Guid id, Guid userId);
+        Task<bool> EditDetailsNotebookAsync(NotebookDetailsViewModel? model, Guid userId);
 
     }
 }
