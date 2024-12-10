1e2f3161d4cdf0f36d58fbc5692ae6ca89bf7d0a
dpiskov <dpiskov02@gmail.com>
2024-12-10 08:54:09 +0200

    Create NotebookConfiguration.cs

---
+++ LunarApp.Data/Configuration/NotebookConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using LunarApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LunarApp.Data.Configuration
+{
+    public class NotebookConfiguration : IEntityTypeConfiguration<Notebook>
+    {
+        public void Configure(EntityTypeBuilder<Notebook> builder)
+        {
+            builder.HasOne(n => n.User)
+                .WithMany()
+                .HasForeignKey(n => n.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(n => n.Folders)
+                .WithOne(f => f.Notebook)
+                .HasForeignKey(f => f.NotebookId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasMany(n => n.Notes)
+                .WithOne(note => note.Notebook)
+                .HasForeignKey(note => note.NotebookId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
