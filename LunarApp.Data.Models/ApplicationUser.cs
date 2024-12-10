d7d62da7bfa6f33f10158bc05dd024e803f4ba12
dpiskov <dpiskov02@gmail.com>
2024-12-10 16:24:24 +0200

    Update ApplicationUser.cs

--- LunarApp.Data.Models/ApplicationUser.cs
+++ LunarApp.Data.Models/ApplicationUser.cs
@@ -9,15 +9,5 @@ namespace LunarApp.Data.Models
             // ReSharper disable once VirtualMemberCallInConstructor
             this.Id = Guid.NewGuid();
         }
-
-        //TODO: Add more properties to user
-
-        //public virtual ICollection<ApplicationUserMovie> ApplicationUserMovies { get; set; }
-        //    = new HashSet<ApplicationUserMovie>();
-
-        //public virtual ICollection<Ticket> Tickets { get; set; }
-        //    = new HashSet<Ticket>();
     }
 }
-
-
