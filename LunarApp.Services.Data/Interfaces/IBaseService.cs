a40f6a0b2f49aa7d162c1a80c7cb3c5b3cc689fb
dpiskov <dpiskov02@gmail.com>
2024-12-10 08:58:00 +0200

    Update IBaseService.cs

--- LunarApp.Services.Data/Interfaces/IBaseService.cs
+++ LunarApp.Services.Data/Interfaces/IBaseService.cs
@@ -4,8 +4,8 @@ namespace LunarApp.Services.Data.Interfaces
 {
     public interface IBaseService
     {
-        Task<FolderNotesViewModel> GetFilteredNotesAsyncByNotebookId(Guid notebookId, Guid? parentFolderId, Guid? folderId, string? searchQuery, string? tagFilter);
-        Task<FolderNotesViewModel> GetFilteredNotesAsync(string? searchQuery, string? tagFilter);
-        Task<IEnumerable<string>> GetAllTagsAsync();
+        Task<FolderNotesViewModel> GetFilteredNotesAsyncByNotebookId(Guid notebookId, Guid? parentFolderId, Guid? folderId, Guid userId, string? searchQuery, string? tagFilter);
+        Task<FolderNotesViewModel> GetFilteredNotesAsync(Guid userId, string? searchQuery, string? tagFilter);
+        Task<IEnumerable<string>> GetAllTagsAsync(Guid userId);
     }
 }
