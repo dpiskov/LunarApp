493f6c92ccc16df3e56ba5609c9eefa6fdd97769
dpiskov <dpiskov02@gmail.com>
2024-12-10 08:54:44 +0200

    Update NoteConfiguration.cs

--- LunarApp.Data/Configuration/NoteConfiguration.cs
+++ LunarApp.Data/Configuration/NoteConfiguration.cs
@@ -8,17 +8,32 @@ namespace LunarApp.Data.Configuration
     {
         public void Configure(EntityTypeBuilder<Note> builder)
         {
-            // Folder to Note relationship: Remove cascade delete to handle it programmatically
-            builder.HasOne(n => n.Folder)  // A Note belongs to one Folder
-                .WithMany(f => f.Notes)  // A Folder has many Notes
-                .HasForeignKey(n => n.FolderId)  // Foreign key from Note to Folder
-                .OnDelete(DeleteBehavior.Restrict); // Use Restrict to prevent automatic cascade delete
+            builder.HasOne(n => n.Folder)
+                .WithMany(f => f.Notes)
+                .HasForeignKey(n => n.FolderId)
+                .OnDelete(DeleteBehavior.Restrict);
 
-            // Notebook to Note relationship (cascade delete)
-            builder.HasOne(n => n.Notebook)  // A Note belongs to one Notebook
-                .WithMany(nb => nb.Notes)  // A Notebook has many Notes
-                .HasForeignKey(n => n.NotebookId)  // Foreign key from Note to Notebook
-                .OnDelete(DeleteBehavior.Cascade);  // Cascade delete when Notebook is deleted
+            builder.HasOne(n => n.Notebook)
+                .WithMany(nb => nb.Notes)
+                .HasForeignKey(n => n.NotebookId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+
+            builder.HasOne(n => n.User)
+                .WithMany()
+                .HasForeignKey(n => n.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            builder.HasOne(n => n.ParentFolder)
+                .WithMany()
+                .HasForeignKey(n => n.ParentFolderId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(n => n.Tag)
+                .WithMany()
+                .HasForeignKey(n => n.TagId)
+                .OnDelete(DeleteBehavior.NoAction);
         }
     }
 }
