19dbe596a5d33a9acc9d2e4547c22b4c32ed685f
dpiskov <dpiskov02@gmail.com>
2024-12-10 09:02:52 +0200

    Update UserService.cs

--- LunarApp.Services.Data/UserService.cs
+++ LunarApp.Services.Data/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using LunarApp.Data.Models;
+using LunarApp.Data.Repository.Interfaces;
 using LunarApp.Services.Data.Interfaces;
 using LunarApp.Web.ViewModels.Admin.UserManagement;
 using Microsoft.AspNetCore.Identity;
@@ -6,8 +7,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LunarApp.Services.Data
 {
-    public class UserService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole<Guid>> roleManager)
-        : IUserService
+    public class UserService(
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole<Guid>> roleManager,
+        IRepository<Notebook, Guid> notebookRepository,
+        IRepository<Folder, Guid> folderRepository,
+        IRepository<Note, Guid> noteRepository,
+        IRepository<Tag, Guid> tagRepository
+        ) : IUserService
     {
         public async Task<IEnumerable<AllUsersViewModel>> GetAllUsersAsync()
         {
@@ -41,7 +48,7 @@ namespace LunarApp.Services.Data
         {
             ApplicationUser? user = await userManager
                 .FindByIdAsync(userId.ToString());
-            var normalizedRoleName = roleName.ToUpperInvariant();
+            string normalizedRoleName = roleName.ToUpperInvariant();
             bool roleExists = await roleManager.RoleExistsAsync(normalizedRoleName);
 
             if (user == null || roleExists == false)
@@ -92,22 +99,37 @@ namespace LunarApp.Services.Data
 
         public async Task<bool> DeleteUserAsync(Guid userId)
         {
-            ApplicationUser? user = await userManager
-                .FindByIdAsync(userId.ToString());
-
+            ApplicationUser? user = await userManager.FindByIdAsync(userId.ToString());
             if (user == null)
             {
                 return false;
             }
 
-            IdentityResult? result = await userManager
-                .DeleteAsync(user);
-            if (result.Succeeded == false)
+            IQueryable<Note> notes = noteRepository.GetAllAttached().Where(n => n.UserId == userId);
+            await noteRepository.DeleteRangeAsync(notes);
+
+            IQueryable<Folder> folders = folderRepository.GetAllAttached().Where(f => f.Notebook.UserId == userId);
+            await folderRepository.DeleteRangeAsync(folders);
+
+            IQueryable<Notebook> notebooks = notebookRepository.GetAllAttached().Where(n => n.UserId == userId);
+            await notebookRepository.DeleteRangeAsync(notebooks);
+
+            IQueryable<Tag> tags = tagRepository.GetAllAttached()
+                .Where(t => t.UserId == userId);
+            await tagRepository.DeleteRangeAsync(tags);
+
+            IdentityResult result = await userManager.DeleteAsync(user);
+            if (result.Succeeded)
             {
-                return false;
+                await noteRepository.SaveChangesAsync();
+                await folderRepository.SaveChangesAsync();
+                await notebookRepository.SaveChangesAsync();
+                await tagRepository.SaveChangesAsync();  
+
+                return true;
             }
 
-            return true;
+            return false;
         }
     }
 }
