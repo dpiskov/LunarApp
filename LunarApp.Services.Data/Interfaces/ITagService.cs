b8c72c961055dde81430e27ac3cd524deb43062f
dpiskov <dpiskov02@gmail.com>
2024-12-10 08:58:34 +0200

    Update ITagService.cs

--- LunarApp.Services.Data/Interfaces/ITagService.cs
+++ LunarApp.Services.Data/Interfaces/ITagService.cs
@@ -5,17 +5,17 @@ namespace LunarApp.Services.Data.Interfaces
 {
     public interface ITagService
     {
-        Task<Tag?> GetByTitleAsync(string name);
+        Task<Tag?> GetByTitleAsync(string name, Guid userId);
 
-        Task<IEnumerable<TagViewModel>> IndexGetAllTagsOrderedByNameAsync();
+        Task<IEnumerable<TagViewModel>> IndexGetAllTagsOrderedByNameAsync(Guid userId);
         Task<TagCreateViewModel> GetCreateTagAsync(Guid? notebookId, Guid? parentFolderId, Guid? folderId,
             Guid? noteId);
-        Task CreateTagAsync(TagCreateViewModel model);
+        Task CreateTagAsync(TagCreateViewModel model, Guid userId);
         Task<TagEditViewModel?> GetTagForEditByIdAsync(Guid? notebookId, Guid? parentFolderId, Guid? folderId,
-            Guid? noteId, Guid tagId);
+            Guid? noteId, Guid tagId, Guid userId);
         Task<bool> EditTagAsync(TagEditViewModel? model);
         Task<TagRemoveViewModel?> GetTagForDeleteByIdAsync(Guid notebookId, Guid? parentFolderId, Guid? folderId,
-            Guid noteId, Guid tagId);
+            Guid? noteId, Guid tagId);
         Task DeleteTagAsync(Guid tagId);
     }
 }
